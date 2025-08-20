4592741776ba3bd8d8b761c2719b42bcb8a1ca77
Peter Zhu <137370991+KONpiGG@users.noreply.github.com>
2025-08-20 13:35:59 +0800

    Use relative import for prompt

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from astrbot.api.event import filter, AstrMessageEvent
 from astrbot.api.star import Context, Star, register
 from astrbot.api import AstrBotConfig, logger
-from prompt import prompt
+from .prompt import prompt
 
 
 @register(
