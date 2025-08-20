c6aa0d262286a6501e9dbd09715fc61bf9be1f0d
Peter Zhu <137370991+KONpiGG@users.noreply.github.com>
2025-08-20 12:54:11 +0800

    prompt

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
+from prompt import prompt
 
 @register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
 class MyPlugin(Star):
